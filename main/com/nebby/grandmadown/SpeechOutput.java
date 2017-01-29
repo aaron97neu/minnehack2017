a5fa9c56b28d633308102e8d0c5d70a1a3cdc328
Killian-LeClainche <kleclain@purdue.edu>
2017-01-28 23:13:28 -0600

    Speech Output finished

--- main/com/nebby/grandmadown/SpeechOutput.java
+++ main/com/nebby/grandmadown/SpeechOutput.java
@@ -10,41 +10,114 @@ public class SpeechOutput
 		speechOutput = new StringBuilder();
 	}
 	
+	/**
+	 * Default way to say some text.
+	 * @param text
+	 */
 	public void text(String text)
 	{
-		
+		speechOutput.append(text);
 	}
 	
+	/**
+	 * Play an mp3 audio piece.
+	 * <audio src="AUDIO_SRC" />
+	 * @param audioSrc : location of the mp3 file
+	 */
 	public void audio(String audioSrc)
 	{
-		
+		speechOutput.append("<audio src=\"");
+		speechOutput.append(audioSrc);
+		speechOutput.append("\" />");
 	}
 	
-	public void pause(Strength strength, int milliseconds)
+	/**
+	 * Pause for determined amount of time before continuing.
+	 * <break strength="STRENGTH_LEVEL" />
+	 * @param strength : enum variable with choices of NONE, VERY_WEAK, WEAK, MEDIUM, STRONG, VERY_STRONG
+	 */
+	public void pause(Strength strength)
 	{
-		
+		speechOutput.append("<break strength=\"");
+		speechOutput.append(strength.content);
+		speechOutput.append("\" />");
 	}
 	
-	public void paragraph(String text)
+	/**
+	 * Pause for determined amount of time before continuing.
+	 * <break time="clamp(TIME_AMOUNT, 0, 10000)" />
+	 * @param milliseconds : time in milliseconds to wait, max is 10 seconds.
+	 */
+	public void pause(int milliseconds)
 	{
-		
+		speechOutput.append("<break time=\"");
+		speechOutput.append(Math.max(Math.min(milliseconds, 10000), 0));
+		speechOutput.append("\" />");
 	}
 	
+	/**
+	 * Initialize paragraph, has strong pause afterwards.
+	 */
+	public void startParagraph()
+	{
+		speechOutput.append("<p>");
+	}
+	
+	/**
+	 * End paragraph with strong pause.
+	 */
+	public void endParagraph()
+	{
+		speechOutput.append("</p>");
+	}
+	
+	/**
+	 * Say content inside a certain way.
+	 * <say-as interpret-as="INTERPRET_TYPE">CONTENT</say-as>
+	 * @param interpret : enum type with choices of SPELT, NUMBER, ORDINAL, DIGITS, FRACTION,
+	 * UNIT, TIME, TELEPHONE, ADDRESS
+	 * @param content : text to be read
+	 */
 	public void say(Interpret interpret, String content)
 	{
-		
+		speechOutput.append("<say-as interpret-as\"");
+		speechOutput.append(interpret.content);
+		speechOutput.append("\">");
+		speechOutput.append(content);
+		speechOutput.append("</say-as>");
 	}
 	
+	/**
+	 * Say content with date format.
+	 * @param format : format in any combination of d (day), m (month), and y (year)
+	 * @param content : date to be read, make sure to include "/" in between portions of date.
+	 */
 	public void date(String format, String content)
 	{
-		
+		speechOutput.append("<say-as interpret-as=\"date\" format=\"");
+		speechOutput.append(format);
+		speechOutput.append("\">");
+		speechOutput.append(content);
+		speechOutput.append("</say-as>");
 	}
 	
-	public void pronouce(Syntax type, String content)
+	/**
+	 * Pronounce text (most likely a single word) in a certain format.
+	 * @param syntax : type of pronounciation with types NOUN, PAST, VERB, OTHER (like bass has two meanings).
+	 * @param content : word to be said.
+	 */
+	public void pronounce(Syntax syntax, String content)
 	{
-		
+		speechOutput.append("<w role=\"");
+		speechOutput.append(syntax.content);
+		speechOutput.append("\">");
+		speechOutput.append(content);
+		speechOutput.append("</w>");
 	}
 	
+	/**
+	 * Call to return the full speech output.
+	 */
 	public String toString()
 	{
 		return "<speak>" + speechOutput.toString() + "</speech>";
@@ -78,7 +151,14 @@ public class SpeechOutput
 	
 	public static enum Syntax
 	{
+		VERB("ivona:VB"), PAST("ivona:VBD"), NOUN("ivona:NN"), OTHER("ivona:SENSE_1");
 		
+		private final String content;
+		
+		private Syntax(String title)
+		{
+			content = title;
+		}
 	}
 	
 }
