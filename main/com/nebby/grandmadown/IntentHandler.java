df613939e31797394692dc644abcc9dd43842f86
Aaron Neustedter <aaron97neu@yahoo.com>
2017-01-28 22:30:03 -0600

    Speechlet stuff

--- main/com/nebby/grandmadown/IntentHandler.java
+++ main/com/nebby/grandmadown/IntentHandler.java
@@ -2,14 +2,17 @@ package com.nebby.grandmadown;
 
 import com.amazon.speech.slu.Intent;
 import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SpeechletResponse;
 
 public class IntentHandler 
 {
 
-	public void pillRequest(Intent intent, Session session)
+	public SpeechletResponse addPill(Intent intent, Session session)
 	{
-		
+		//SpeechletResponse toReturn = //SpeechletResponse.newAskResponse(outputSpeech, reprompt, card)
+		return null;
 	}
+	
 	
 	
 	
